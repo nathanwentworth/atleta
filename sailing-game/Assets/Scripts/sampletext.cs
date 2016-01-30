21bca30bbfa515f7130036266ea2b5f966431b95
Nathan Wentworth <nwentworth95@gmail.com>
2016-01-30 10:34:40 -0500

    Revert "added gameobjects"
    # Conflicts:
    #	sailing-game/Assets/Scenes/main.unity
    #	sailing-game/Library/InspectorExpandedItems.asset
    #	sailing-game/Library/ScriptAssemblies/Assembly-CSharp.dll
    #	sailing-game/Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    #	sailing-game/Library/assetDatabase3
    #	sailing-game/Library/metadata/00/00000000000000004000000000000000
    #	sailing-game/Library/metadata/00/0000000000000000c000000000000000
    #	sailing-game/Temp/__Backupscenes/0.backup

--- sailing-game/Assets/Scripts/sampletext.cs
+++ sailing-game/Assets/Scripts/sampletext.cs
@@ -5,8 +5,6 @@ using UnityEngine.UI;
 public class sampletext : MonoBehaviour {
   private int splitNum;
 
-	public GameObject book;
-
   public Text ans1text;
   public Text ans2text;
   public Text ans3text;
@@ -17,12 +15,7 @@ public class sampletext : MonoBehaviour {
   public Button ans2;
   public Button ans3;
 
-public GameObject ans1obj;
-public GameObject ans2obj;
-public GameObject ans3obj;
-
-
-private bool ans1interactable;
+  private bool ans1interactable;
   private bool ans2interactable;
   private bool ans3interactable;
 
@@ -52,10 +45,6 @@ private bool ans1interactable;
 
   private void IntroScript() {  
     dialogueText.text = "This is a sample block of text! If this is showing up, that means the script is working properly. I want to make this long enough so that it actually fills at least a few lines. Now to test how \nline breaks \nwork i'm just gonna throw some in. \nokay cool let's see how that looks!";
-	
-	ans1obj.SetActive (true);
-	ans2obj.SetActive (true);
-	ans3obj.SetActive (true);
 
     ans1.onClick.AddListener(Script1A);
     ans2.onClick.AddListener(Script1B);
@@ -69,10 +58,6 @@ private bool ans1interactable;
   private void Script1A() {
     dialogueText.text = "This is more sample text! if this shows up, then you clicked a button and it properly called the second dialogue bit. ";
 
-	ans1obj.SetActive (true);
-	ans2obj.SetActive (true);
-	ans3obj.SetActive (false);
-
     ans1.onClick.AddListener(Script2A);
     ans2.onClick.AddListener(Script2B);
     ans3.onClick.AddListener(Script1A);
@@ -85,10 +70,6 @@ private bool ans1interactable;
   private void Script1B() {
     dialogueText.text = "This is a second dialogue choice. it'll show you different dialogue than the first choice, which is A ";
 
-	ans1obj.SetActive (true);
-	ans2obj.SetActive (true);
-	ans3obj.SetActive (true);
-
     ans1.onClick.AddListener(Script1A);
     ans2.onClick.AddListener(Script1B);
     ans3.onClick.AddListener(Script1A);
@@ -100,10 +81,6 @@ private bool ans1interactable;
   private void Script2A() {
     dialogueText.text = "You entered the trigger and this is the first option of the first split";
 
-	ans1obj.SetActive (true);
-	ans2obj.SetActive (true);
-	ans3obj.SetActive (true);
-
     ans1.onClick.AddListener(Script1A);
     ans2.onClick.AddListener(Script1B);
     ans3.onClick.AddListener(Script1A);
@@ -114,10 +91,6 @@ private bool ans1interactable;
   }
   private void Script2B() {
     dialogueText.text = "You entered the trigger and this is the second option of the first split";
-
-	ans1obj.SetActive (true);
-	ans2obj.SetActive (true);
-	ans3obj.SetActive (true);
 
     ans1.onClick.AddListener(Script1A);
     ans2.onClick.AddListener(Script1B);
